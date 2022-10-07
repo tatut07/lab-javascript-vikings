8db0bf69178dde24112a7b7a8affc1c99f6cb79e
Tatiana Toledo <tatiana@tatianas-mbp.speedport.ip>
2022-10-07 12:57:12 +0200

    iteration4Bonus

--- src/viking.js
+++ src/viking.js
@@ -40,6 +40,34 @@ class Saxon extends Soldier {
     }
   }
 }
-
 // War
-class War {}
+class War {
+  vikingArmy = [];
+  saxonArmy = [];
+  addViking(Viking) {
+    this.vikingArmy.push(Viking);
+  }
+  addSaxon(Saxon) {
+    this.saxonArmy.push(Saxon);
+  }
+  vikingAttack() {
+    const attacker = Math.floor(Math.random()) * this.vikingArmy.length;
+    const victim = Math.floor(Math.random()) * this.saxonArmy.length;
+    const damage = this.vikingArmy[attacker].attack();
+    const saxonDam = this.saxonArmy[victim].receiveDamage(damage);
+    if (this.saxonArmy[victim].health <= 0) {
+      this.saxonArmy.splice(victim, 1);
+    }
+    return saxonDam;
+  }
+  saxonAttack() {
+    const attacker = Math.floor(Math.random()) * this.saxonArmy.length;
+    const victim = Math.floor(Math.random()) * this.vikingArmy.length;
+    const damage = this.saxonArmy[attacker].attack();
+    const vikingDam = this.vikingArmy[victim].receiveDamage(damage);
+    if (this.vikingArmy[victim].health <= 0) {
+      this.vikingArmy.splice(victim, 1);
+    }
+    return vikingDam;
+  }
+}
